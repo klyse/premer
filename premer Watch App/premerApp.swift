2ccc8dfb921ba6107ac8bd2a13408f55e0d011bc
klyse <Klaus.Pruenster@gmail.com>
2023-05-05 20:57:46 +0200

    Initial commit

---
+++ premer Watch App/premerApp.swift
@@ -0,0 +1,17 @@
+//
+//  premerApp.swift
+//  premer Watch App
+//
+//  Created by Klaus on 2023-05-05.
+//
+
+import SwiftUI
+
+@main
+struct premer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ premer/premerApp.swift
@@ -0,0 +1,17 @@
+//
+//  premerApp.swift
+//  premer
+//
+//  Created by Klaus on 2023-05-05.
+//
+
+import SwiftUI
+
+@main
+struct premerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
