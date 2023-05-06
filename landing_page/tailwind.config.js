ff561175cf9326eb1453147c28605463441bcc86
klyse <Klaus.Pruenster@gmail.com>
2023-05-06 15:19:37 +0200

    fix text colors

--- landing_page/tailwind.config.js
+++ landing_page/tailwind.config.js
@@ -1,15 +1,17 @@
 module.exports = {
   content: ["./components/**/*.js", "./pages/**/*.js"],
   theme: {
-    colors: {
-      white: "#FFFFFF",
-      turquoise: "#29D3DA",
-      blue1: "#0077FF",
-      blue2: "#064093",
-      blue3: "#084698",
-      beige: "#E0B386",
-      pink1: "#DA498C",
-      pink2: "#961484",
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        turquoise: "#29D3DA",
+        blue1: "#0077FF",
+        blue2: "#064093",
+        blue3: "#084698",
+        beige: "#E0B386",
+        pink1: "#DA498C",
+        pink2: "#961484",
+      },
     },
   },
   plugins: [],
