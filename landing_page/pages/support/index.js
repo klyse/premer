ae54efa3f421a38a85b4dda469591c2007e4459d
klyse <Klaus.Pruenster@gmail.com>
2023-05-06 12:43:43 +0200

    added support page

---
+++ landing_page/pages/support/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 class="text-2xl">premer Support</h1>
+      <div class="divider" />
+      <span>
+        If you need support, please feel free to reach out to us at 
+        <a href="mailto:support@klyse.eu">support@klyse.eu</a>. We are
+        dedicated to providing you with the best possible assistance and will do
+        our best to respond to your inquiry as soon as possible. We are looking
+        forward to hearing from you!
+      </span>
+    </div>
+  );
+}
