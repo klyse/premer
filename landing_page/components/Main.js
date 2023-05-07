0e6dc63b8995ce927f5064be34489bd2b9c9c324
klyse <Klaus.Pruenster@gmail.com>
2023-05-07 09:40:18 +0200

    added email signup

--- landing_page/components/Main.js
+++ landing_page/components/Main.js
@@ -61,25 +61,28 @@ export default function Main() {
       <section className="relative">
         <div className="max-w-6xl mx-auto px-4 sm:px-6 text-center">
           <div className="py-24 md:py-36">
-            <h1 className="mb-5 text-6xl Avenir font-semibold">
-              Subscribe to our newsletter
-            </h1>
-            <h1 className="mb-9 text-2xl font-semibold">
-              Enter your email address and get our newsletters straight away.
-            </h1>
-            <input
-              placeholder="jack@example.com"
-              name="email"
-              type="email"
-              autoComplete="email"
-              className="border border-gray-600 w-1/4 pr-2 pl-2 py-3 mt-2 rounded-md font-semibold hover:border-gray-900"
-            ></input>{" "}
-            <a
-              className="inline-flex items-center px-14 py-3 mt-2 ml-2 font-medium transition duration-500 ease-in-out transform bg-transparent border rounded-lg bg-gray-900"
-              href="/"
-            >
-              <span className="justify-center">Subscribe</span>
-            </a>
+            <form action="https://formspree.io/f/mqkonngo" method="POST">
+              <h1 className="mb-5 text-6xl Avenir font-semibold">
+                Subscribe to our newsletter
+              </h1>
+              <h1 className="mb-9 text-2xl font-semibold">
+                Enter your email address and get our newsletters straight away.
+              </h1>
+              <input
+                placeholder="jack@example.com"
+                name="email"
+                type="email"
+                autoComplete="email"
+                className="border border-gray-600 w-1/4 pr-2 pl-2 py-3 mt-2 rounded-md font-semibold hover:border-gray-900 text-black"
+              ></input>{" "}
+              <button
+                type="submit"
+                className="inline-flex items-center px-14 py-3 mt-2 ml-2 font-medium transition duration-500 ease-in-out transform bg-transparent border rounded-lg bg-gray-900"
+                href="/"
+              >
+                <span className="justify-center">Subscribe</span>
+              </button>
+            </form>
           </div>
         </div>
       </section>
